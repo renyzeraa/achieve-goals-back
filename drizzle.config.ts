889f5b44858c704957ce29741d31abb86e6cdb3f
Renan Silva <renansilvaytB@gmail.com>
2024-09-10 20:50:10 -0300

    feat: database environment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'drizzle-kit'
+import { env } from './src/env'
 
 export default defineConfig({
   schema: "./schema.ts",
+  out: "./.migrations",
   dialect: 'postgresql',
-  migrations: {
-    prefix: 'supabase'
+  dbCredentials: {
+    url: env.DATABASE_URL,
   }
 })
