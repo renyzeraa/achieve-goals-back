a4d5d26aa1ee9a73e53b4754589069ba2c745390
Renan Silva <renansilvaytB@gmail.com>
2024-09-16 19:53:37 -0300

    feat: validado com fastify-type-provider-zod

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,15 +1,24 @@
 import fastify from "fastify"
 import { createGoal } from "../functions/create-goal"
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod"
 import z from "zod"
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.post('/goals', async (request) => {
-  const goalSchema = z.object({
-    desiredWeeklyFrequency: z.number().int().min(1).max(7),
-    title: z.string()
-  })
-  const { desiredWeeklyFrequency, title } = goalSchema.parse(request.body)
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post('/goals', {
+  schema: {
+    body: z.object({
+      desiredWeeklyFrequency: z.number().positive(),
+      title: z.string().min(1)
+    })
+  }
+
+}, async (request) => {
+  const { desiredWeeklyFrequency, title } = request.body
+
   await createGoal({
     desiredWeeklyFrequency,
     title
